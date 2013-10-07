7539a87218742af39f77ebb2055462d4c7a82ddf
SamanthaDegges <samanthadegges@samanthadeggess-MacBook-Pro.local>
2013-10-07 10:02:28 -0700

    cleaned up strings and typography
    I addded spaces, new lines and breaks to make the output more readable
    and gamelike.

--- MASHIP.py
+++ MASHIP.py
@@ -4,42 +4,42 @@ print """
 >>> Type your answers the following questions to determine your fate...
 """ 
 
-partner1 = raw_input(">>> Who do you like? ")
-partner2 = raw_input(">>> Name a celebrity you'd like to date. ")
-partner3 = raw_input(">>> What's the name of the person who is nearest to you? ")
-partner4 = raw_input(">>> Name a person who likes you. ")
+partner1 = raw_input("Who do you like? \n>>> ")
+partner2 = raw_input("Name a celebrity you'd like to date. \n>>> ")
+partner3 = raw_input("What's the name of the person who is nearest to you? \n>>> ")
+partner4 = raw_input("Name a person who likes you. \n>>> ")
 
-print "\n>>> Well... let's figure out where you could live. "
-place1 = raw_input(">>> Name a place that you've always wanted to visit. ")
-place2 = raw_input(">>> Name a place you visited and absolutely hated. ")
-place3 = raw_input(">>> What's your hometown? ")
-place4 = raw_input(">>> That's a nice place to be from. It explains a lot. \n\n >>> Just to shake things up, what country would you like to avoid living in? ")
+print "\nWell... let's figure out where you could live."
+place1 = raw_input("Name a place that you've always wanted to visit. \n>>> ")
+place2 = raw_input("Name a place you visited and absolutely hated. \n>>> ")
+place3 = raw_input("What's your hometown? \n>>> ")
+place4 = raw_input("\nThat's a nice place to be from. It explains a lot. \n Just to shake things up, what country would you like to avoid living in? \n>>> ")
 
 print """
->>> Did you know that M.A.S.H.I.P. is an acronym?
+Did you know that M.A.S.H.I.P. is an acronym?
     \t M is for mansion
     \t A is for apartment
     \t S is for shack
     \t H is for house
     \t I is for igloo
     \t P is for palace
->>> Today you will learn which one of these you will end up living in. But first, you need to provide some numerical data...
+ Today you will learn which one of these you will end up living in. But first, \n you need to provide some numerical data...
 """
 
-number1 = raw_input(">>> What is your favorite number? ")
-number2 = raw_input(">>> How many pets do you have? ")
-number3 = raw_input(">>> How many years old are you? ")
+number1 = raw_input("What is your favorite number? \n>>> ")
+number2 = raw_input("How many pets do you have? \n>>> ")
+number3 = raw_input("How many years old are you? \n>>> ")
 
-number4 = raw_input("I need to know these numbers so that I can guess how many children you will have! \nHow many would you like to have? ")
+number4 = raw_input("I need to know these numbers so that I can guess how many children you will have! How many would you like to have? \n>>> ")
 
 print """
 In case you have 100 kids, 
 you're going to be working nonstop. 
 No time to kiss %s or possibly %s.""" % (partner1, partner2)
 
-job1 = raw_input("What was the title of your first job? ")
-job2 = raw_input("What is the job title you've always wanted? ")
-job3 = raw_input("What is %s's job? " %partner2) 
+job1 = raw_input("\nWhat was the title of your first job? \n>>> ")
+job2 = raw_input("What is the job title you've always wanted? \n>>> ")
+job3 = raw_input("What is %s's job? \n>>> " %partner2) 
 job4= "a clerk at the Department of Motor Vehicles"
 
 #Lists
@@ -60,9 +60,9 @@ def ultimate(list):
 
 
 print """
-Ok! I've talked to the gods and found out all about your future. I hope you're content with the results. 
+  ---Ok! I've talked to the gods and found out all about your future.--- \n  I hope you're content with the results. 
 	\t* You're going to marry %s.
 	\t* You're going to live in %s in %s.
-	\t* You're going to work as %s
-	\t* You're going to have %r of children! Congratulations!
+	\t* You're going to work as '%s'
+	\t* You're going to have %r children! Congratulations!
 Thanks for playing M.A.S.H.I.P.""" % (ultimate(partner), ultimate(maship), ultimate(place), ultimate(job), ultimate(number))
